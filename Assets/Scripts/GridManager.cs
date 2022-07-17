e010a6cf46df7fb509e7c4cf382b029ac4ffa8dc
MrFMS <mathieu.formosa@gmail.com>
2022-07-17 12:08:00 +0200

    Add new row/line

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -12,22 +12,34 @@ public class GridManager : MonoSingleton<GridManager>
 
     public List<Ground> l_ground = new List<Ground>();
 
+
+    GameObject newSlab;
+    private int xMax;
+    private int zMax;
+
     private void Start()
     {
+        xMax = squareGridDimension;
+        zMax = squareGridDimension;
         GenerateSlabList();
-        //CreateGrid();
     }
 
-    private void GenerateSlabList()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            NewGridLine();
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            NewGridRow();
+    }
 
-
+    public void GenerateSlabList()
+    {
 
         for (int x = 0; x < squareGridDimension; x++)
         {
             for (int z = 0; z < squareGridDimension; z++)
             {
-                GameObject newSlab;
+                
                 if (x == 0 && z == 0)
                 {
                     newSlab = Instantiate(startPrefab, transform);
@@ -41,7 +53,6 @@ public class GridManager : MonoSingleton<GridManager>
                 l_ground.Add(newSlab.GetComponent<Ground>());
                 }
 
-
                 newSlab.transform.position = new Vector3(x, 0, z);
                 newSlab.name = x + " , " + z;
 
@@ -49,9 +60,27 @@ public class GridManager : MonoSingleton<GridManager>
         }
     }
 
-    private void CreateGrid()
+    private void NewGridRow()
     {
-
+        for (int x = 0; x < xMax; x++)
+        {
+            var randomSlab = prefab[Random.Range(0, prefab.Length)];
+            newSlab = Instantiate(randomSlab, transform);
+            newSlab.transform.position = new Vector3(x, 0, zMax);
+            newSlab.name = x + " , " + zMax;
+        }
+        zMax++;
     }
 
+    private void NewGridLine()
+    {
+        for (int z = 0; z < zMax; z++)
+        {
+            var randomSlab = prefab[Random.Range(0, prefab.Length)];
+            newSlab = Instantiate(randomSlab, transform);
+            newSlab.transform.position = new Vector3(xMax, 0, z);
+            newSlab.name = xMax + " , " + z;
+        }
+        xMax++;
+    }
 }
