2e450150ee80313efa752ce78167bb773a183b52
MrFMS <mathieu.formosa@gmail.com>
2022-07-17 17:22:29 +0200

    music stuff en bordel

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeBGM(AudioClip music)
+    {
+        backgroundMusic.Stop();
+        backgroundMusic.clip = music;
+        backgroundMusic.Play();
+    }
+}
